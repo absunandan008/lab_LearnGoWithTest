89b45a094f2bcfc4216846f0e42360e1543069d1
absunandan008 <absunandan008@gmail.com>
2021-12-19 15:31:10 -0500

    Hello World Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/absunandan008/lab_LearnGoWithTest
+
+go 1.17
---
+++ hello/hello_test.go
@@ -0,0 +1,70 @@
+package main
+
+import "testing"
+
+/*
+First Test
+func TestHello(t *testing.T) {
+    got := Hello()
+    want := "Hello, world"
+
+    if got != want {
+        t.Errorf("got %q want %q", got, want)
+    }
+}
+*/
+/*
+Second Iteration
+func TestHello(t *testing.T) {
+	t.Run("saying hello to folks", func(t *testing.T) {
+		got := Hello(("ABC"))
+		want := "Hello, ABC"
+
+		if got != want {
+			t.Errorf("got %q anf want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q and want %q", got, want)
+		}
+	})
+}
+*/
+//Third Iteration
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		//t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "Bonjour, Lauren"
+		assertCorrectMessage(t, got, want)
+	})
+
+}

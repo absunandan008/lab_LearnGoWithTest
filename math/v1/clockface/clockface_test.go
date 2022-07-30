f7bf68e8d740e5073e195e3a70e4f9ae1d990126
absunandan008 <absunandan008@gmail.com>
2022-07-30 09:44:37 -0400

    commiting math v1..v3 divide by zero issue and radian math.pi return

---
+++ math/v1/clockface/clockface_test.go
@@ -0,0 +1,18 @@
+package clockface_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/absunandan008/lab_LearnGoWithTest/math/v1/clockface"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v but want %v", got, want)
+	}
+}

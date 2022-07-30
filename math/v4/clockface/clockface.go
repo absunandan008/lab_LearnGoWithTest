2e8c1d4aa9565967a363a89097bac8b6850e92b5
absunandan008 <absunandan008@gmail.com>
2022-07-30 18:08:38 -0400

    commiting math v4  check by approximation

--- math/v4/clockface/clockface.go
+++ math/v4/clockface/clockface.go
@@ -17,6 +17,13 @@ func SecondHand(t time.Time) Point {
 	return Point{150, 60}
 }
 
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+	return Point{x, y}
+}
+
 func secondsInRadians(t time.Time) float64 {
 	//	return float64(t.Second()) * (math.Pi / 30)
 	// to pass floating number testing
